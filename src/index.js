5fe3f6a3d0c7c5137dcad0c585c234b1b80b8ea8
Josh Lankford <lankford.josh@gmail.com>
2016-10-07 16:58:13 -0700

    remove take a seat

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import BeboReact from 'bebo-react';
 import App from './js/components/App.jsx';
 import './style.scss';
 
-document.domain = "blab-dev.im";
-
 BeboReact.render(
   <App />,
   document.getElementById('app'),
