e3c04c66ad7285566578b993fcd423df19b74010
Josh Lankford <lankford.josh@gmail.com>
2016-10-06 23:21:52 -0700

    test for web

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import BeboReact from 'bebo-react';
 import App from './js/components/App.jsx';
-
 import './style.scss';
+
+document.domain = "bebo-dev.im";
+
 BeboReact.render(
   <App />,
   document.getElementById('app'),
