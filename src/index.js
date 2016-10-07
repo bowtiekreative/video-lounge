a945a2ecccc8feea4b3b74856211df13a9dcab6c
Josh Lankford <lankford.josh@gmail.com>
2016-10-06 23:24:34 -0700

    new build correct domain

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import BeboReact from 'bebo-react';
 import App from './js/components/App.jsx';
 import './style.scss';
 
-document.domain = "bebo-dev.im";
+document.domain = "blab-dev.im";
 
 BeboReact.render(
   <App />,
