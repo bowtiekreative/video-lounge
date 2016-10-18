e5186cad3fea851f531bd1c926a6e4954f2ea3bb
derekfidler <df@derekfidler.com>
2016-10-18 13:07:59 -0700

    increase chat length

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -54,7 +54,7 @@ class App extends React.Component {
     setTimeout(() => {
       this.setState({messages: this.state.messages.filter((m)=> m.timestamp !== message.message.timestamp)
       })
-    }, 6000)
+    }, 18000)
   }
 
   handleCallerChanges() {
